145f46d1305bf48c1982a2d6224975aea9629334
Charlotte Walker <charlielizzy@me.com>
2022-07-29 13:55:43 +0100

    deploying to heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
+console.log(process.env.PORT);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
