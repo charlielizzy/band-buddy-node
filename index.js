6166851f05b86632f5bc421724e18776fa1a2a07
Charlotte Walker <charlielizzy@me.com>
2022-07-29 16:34:20 +0100

    api call working

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+// import fetch from "node-fetch";
+// import FormData from "form-data";
+import fs from "fs";
+import dotenv from "dotenv";
+import axios from "axios";
+import multer from "multer";
+
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3001;
 console.log(process.env.PORT);
+const port = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    console.log("filename");
+    cb(null, file.originalname);
+  },
+  destination: function (req, file, cb) {
+    console.log("storage");
+    cb(null, "./uploads");
+  },
+});
+
+const upload = multer({ storage });
+app.post("/audio_info", upload.single("file"), async (req, res) => {
+  console.log("req.file", req.file);
+  const payload = {
+    api_token: process.env.AUDDIO_API_TOKEN,
+    file: fs.createReadStream(req.file.path),
+    return: "spotify",
+  };
+  try {
+    const result = await axios({
+      method: "post",
+      url: "https://api.audd.io/",
+      data: payload,
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    const { data } = result;
+    res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
