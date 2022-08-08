226cd144100e2584d055726a7f40ab3b29a8ca8d
Charlotte Walker <charlielizzy@me.com>
2022-08-08 20:10:38 +0100

    endpoint to get auth token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import fs from "fs";
 import dotenv from "dotenv";
 import axios from "axios";
 import multer from "multer";
+import qs from "qs";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ console.log(process.env.PORT);
 const port = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -46,6 +48,29 @@ app.post("/audio_info", upload.single("file"), async (req, res) => {
     });
     const { data } = result;
     res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/spotify-auth", async (req, res) => {
+  const payload = {
+    grant_type: "authorization_code",
+    code: req.body.code,
+    redirect_uri: process.env.REDIRECT_URI,
+  };
+  try {
+    const result = await axios({
+      method: "post",
+      url: "https://accounts.spotify.com/api/token",
+      data: qs.stringify(payload),
+      headers: {
+        Authorization: `Basic ${process.env.CLIENT_DETAILS}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    res.send(result.data);
+    console.log("result.data", result.data);
   } catch (error) {
     console.log(error);
   }
